75ff26286f5fd7f6922ca25b52a907f6ddb63afa
a31550 <a31550@alunos.ipca.pt>
2026-05-06 12:17:01 +0100

    main clean up

--- src/main.cpp
+++ src/main.cpp
@@ -44,3 +44,90 @@ A estrutura deste ficheiro deve ser simples e legível, permitindo perceber
 rapidamente o pipeline completo de processamento de cada frame.
 ===============================================================================
 */
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <opencv2\opencv.hpp>
+#include <opencv2\core.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\videoio.hpp>
+
+using namespace cv;
+using namespace std;
+
+extern "C"
+{
+#include "../include/vc.h"
+}
+
+int main(void)
+{
+
+  char videofile[20] = "video.avi";
+
+  VideoCapture capture;
+
+  struct
+  {
+    int width, height;
+    int nTotalFrames;
+    int fps;
+    int nFrame;
+  } video;
+
+  // Leitura do vídeo em questão
+  capture.open(videofile);
+
+  if (!capture.isOpened())
+  {
+
+    cerr << "Erro ao abrir o ficheiro de vídeo!\n";
+    return 1;
+  }
+
+  // Número total de frames no vídeo
+  video.nTotalFrames = (int)capture.get(CAP_PROP_FRAME_COUNT);
+
+  // Frame rate do vídeo
+  video.fps = (int)capture.get(CAP_PROP_FPS);
+
+  // Resolução do vídeo
+  video.width = (int)capture.get(CAP_PROP_FRAME_WIDTH);
+  video.height = (int)capture.get(CAP_PROP_FRAME_HEIGHT);
+
+  Mat frame;
+
+  // Inicio do vídeo
+  while (key != 'q')
+  {
+
+    // Verifica a leitura da frame
+    if (!capture.read(frame))
+      break;
+
+    video.nFrame = (int)capture.get(CAP_PROP_POS_FRAMES);
+
+    // Criação da imagem e tradução Mat -> IVC
+    IVC *image = vc_image_new(video.width, video.height, 3, 255);
+    memcpy(image->data, frame.data, video.width * video.height * 3);
+
+
+    // Nosso código
+
+    // Fim nosso código
+
+
+    // Tradução IVC -> Mat
+    memcpy(frame.data, image->data, video.width * video.height * 3);
+    vc_image_free(image);
+
+    imshow("VC - VIDEO", frame);
+
+    key = waitKey(25);
+  }
+
+  capture.release();
+
+  return 0;
+}
