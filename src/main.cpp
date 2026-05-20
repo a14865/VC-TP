5c3187124264902ce92976a02bcae1a1f01fbf43
22552-RaulRibeiro <rauloctober7@hotmail.com>
2026-05-20 10:14:40 +0100

    WIP for orange identification

--- src/main.cpp
+++ src/main.cpp
@@ -84,6 +84,8 @@ int main(void)
     // Inicio do vídeo
     while (key != 'q')
     {
+        OVC orangeBlobs[20] = {0}; // Array para armazenar os blobs de laranja detectados no frame atual
+
         // Reset da contagem de laranjas por frame
         int nOrangesPerFrame = 0;
 
@@ -122,44 +124,30 @@ int main(void)
                 // Se houver blobs mais pequenos que 60000px ignora
                 if (blobs[i].area < 60000) continue;
 
+                if(orangeBlobs[nOrangesPerFrame].perimeter < blobs[i].perimeter && blobs[i].perimeter < 1400) {
+                    orangeBlobs[nOrangesPerFrame] = blobs[i];
+                } else {
+                    orangeBlobs[nOrangesPerFrame].xc = blobs[i].xc;
+                    orangeBlobs[nOrangesPerFrame].yc = blobs[i].yc;
+                    orangeBlobs[nOrangesPerFrame].x = blobs[i].x;
+                    orangeBlobs[nOrangesPerFrame].y = blobs[i].y;
+                }
+                
+                // printf("Blob %d: Area = %d, Perimetro = %d\n", blobs[i].label, blobs[i].area, blobs[i].perimeter);
+                
                 // Contagem de laranjas por frame
                 nOrangesPerFrame++;
 
                 vc_draw_bounding_box_all_blobs(image, &blobs[i], 1, 3, 5, 255, 0, 0);
 
                 // ATIVAÇÃO: Só processa e desenha a Bounding Box se passar a linha
-                if(blobs[i].yc > linhaAtivacao) {
-                  // Mostrar o centro de massa apenas enquanto a laranja estiver entre as linhas de ativação
-                  if(blobs[i]. yc < video.height - linhaAtivacao){
+                if(blobs[i].yc > linhaAtivacao && blobs[i].yc < video.height - linhaAtivacao) {
+                    // Mostrar o centro de massa apenas enquanto a laranja estiver entre as linhas de ativação
                     vc_draw_center_mass_all_blobs(image, &blobs[i], 1, 11, 3, 0, 0, 0);
-                  }
-
-                  double defPrevX = fabs(prevX - blobs[i].xc);
-                  double defPrevY = fabs(prevY - blobs[i].yc);
-
-                  if(defPrevX < 20) {
-                    blobs[i].label = uniqueBlobLabel;
-                  } else {
-                    uniqueBlobLabel++;
-                    blobs[i].label = uniqueBlobLabel;
-                  }
-
-                  prevX = blobs[i].xc;
-                  prevY = blobs[i].yc;
-
-                  if(blobsArea[uniqueBlobLabel] <= 0) blobsArea[uniqueBlobLabel] = blobs[i].area;
-                  if(blobsPerimeter[uniqueBlobLabel] <= 0) blobsPerimeter[uniqueBlobLabel] = blobs[i].perimeter;
-
-                  printf("Blob %d - Label: %d, Area: %d, Perimetro: %d\n", i, blobs[i].label, blobsArea[uniqueBlobLabel], blobsPerimeter[uniqueBlobLabel]);
-
-                //   if(blobsArea[blobs[i].label] < blobs[i].area || blobsPerimeter[blobs[i].label] < blobs[i].perimeter) {
-                //     blobsArea[blobs[i].label] = blobs[i].area;
-                //     blobsPerimeter[blobs[i].label] = blobs[i].perimeter;
-                //   } 
                 }
 
                 // DETECÇÃO: Verifica se o centro de massa do blob está dentro da linha de detecção (margem de 5px para evitar falhas de detecção)
-                if(blobs[i].yc >= (linhaDetecao - 5) && blobs[i].yc <= (linhaDetecao + 5)) {
+                if(blobs[i].yc >= (linhaAtivacao - 5) && blobs[i].yc <= (linhaAtivacao + 5)) {
                   // Se a última detecção de laranja foi há mais de 10 frames, conta como uma nova laranja
                   if(lastOrangeFrame == 0 || (video.nFrame - lastOrangeFrame) > 10) {
                     lastOrangeFrame = video.nFrame;
@@ -173,19 +161,20 @@ int main(void)
         memcpy(frameRGB.data, image->data, video.width * video.height * 3);
         cvtColor(frameRGB, frame, COLOR_RGB2BGR);
 
-        for(int i = 0; i < nlabels; i++) {
+        printf("Oranges in frame: %d\n", nOrangesPerFrame);
 
-            // Se houver blobs mais pequenos que 60000px ignora
-            if (blobs[i].area < 60000) continue;
+        for(int i = 0; i < nOrangesPerFrame; i++) {
+            // printf("nOrangesPerFrame: %d\n", nOrangesPerFrame);
+            // printf("Blob %d: Area = %d, Perimetro = %d\n", orangeBlobs[i].label, orangeBlobs[i].area, orangeBlobs[i].perimeter);
 
-            if(blobs[i].yc > linhaAtivacao) {
-                std::string strArea = std::string("Area: ").append(std::to_string(blobsArea[blobs[i].label])).append("px");
-                cv::putText(frame, strArea, cv::Point(blobs[i].x + (blobs[i].width / 2), blobs[i].y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 0), 2);
-                cv::putText(frame, strArea, cv::Point(blobs[i].x + (blobs[i].width / 2), blobs[i].y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255), 1);
+            if(orangeBlobs[i].yc > linhaAtivacao) {
+                std::string strArea = std::string("Area: ").append(std::to_string(orangeBlobs[i].area)).append("px");
+                cv::putText(frame, strArea, cv::Point(orangeBlobs[i].x + (orangeBlobs[i].width / 2), orangeBlobs[i].y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 0), 2);
+                cv::putText(frame, strArea, cv::Point(orangeBlobs[i].x + (orangeBlobs[i].width / 2), orangeBlobs[i].y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255), 1);
 
-                std::string strPerimetro = std::string("Perimetro: ").append(std::to_string(blobsPerimeter[blobs[i].label])).append("px");
-                cv::putText(frame, strPerimetro, cv::Point(blobs[i].x + (blobs[i].width / 2), blobs[i].y - 50), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 0), 2);
-                cv::putText(frame, strPerimetro, cv::Point(blobs[i].x + (blobs[i].width / 2), blobs[i].y - 50), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255), 1);
+                std::string strPerimetro = std::string("Perimetro: ").append(std::to_string(orangeBlobs[i].perimeter)).append("px");
+                cv::putText(frame, strPerimetro, cv::Point(orangeBlobs[i].x + (orangeBlobs[i].width / 2), orangeBlobs[i].y - 50), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 0), 2);
+                cv::putText(frame, strPerimetro, cv::Point(orangeBlobs[i].x + (orangeBlobs[i].width / 2), orangeBlobs[i].y - 50), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255), 1);
             }
         }
 
